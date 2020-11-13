d057efbf1021f7b92866b9f890e9e2ad1b17394e
Vilandas <d00218436@student.dkit.ie>
2020-11-13 11:55:25 +0000

    Adds timer
    Timer class takes in length of time in miliseconds.
    To start/reset, use StartTime() method.

---
+++ GDLibrary/Core/Parameters/Time/Timer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GDLibrary.Core.Parameters.Time
+{
+    public class Timer
+    {
+        #region Fields
+
+        private double timerLengthMS;
+        private double startTime;
+        private double elapsedTime;
+
+        #endregion
+
+        #region Fields
+
+        public double TimerLengthMS
+        { 
+            get { return timerLengthMS; } 
+            set { timerLengthMS = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Timer(double timerLengthMS)
+        {
+            this.timerLengthMS = timerLengthMS;
+        }
+
+        #endregion
+
+        public void StartTimer(GameTime gameTime)
+        {
+            startTime = gameTime.TotalGameTime.TotalMilliseconds;
+            elapsedTime = 0;
+        }
+
+        public bool IsDone(GameTime gameTime)
+        {
+            elapsedTime = gameTime.TotalGameTime.TotalMilliseconds - startTime;
+            return elapsedTime >= timerLengthMS;
+        }
+    }
+}
